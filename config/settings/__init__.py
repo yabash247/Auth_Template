e38a1e354d32068bd965b9e0eb48d5d19f668bb6
yabash247 <yabashllc@gmail.com>
2025-10-15 15:27:10 -0500

    Remove embedded config for submodule conversion

--- config/settings/__init__.py
+++
@@ -1,1 +0,0 @@
-# Intentionally empty; import from config.settings.base or specific env modules.
--- config/settings/staging.py
+++
@@ -1,3 +0,0 @@
-from .base import *
-import os, environ
-environ.Env.read_env(os.path.join(BASE_DIR, "env", ".env.staging"))
